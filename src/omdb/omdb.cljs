6d1b3f120c420c29707ba100ef2dc4ac32320e36
Nathan Kleekamp <nkleekamp@gmail.com>
2019-03-30 16:03:49 -0400

    add google closure library

--- src/omdb/omdb.cljs
+++ src/omdb/omdb.cljs
@@ -1,56 +1,58 @@
 (ns omdb.omdb
-  (:require [ajax.core :as ajax]
+  (:require [goog.dom :as dom]
+            [goog.dom.forms :as forms]
+            [goog.events :as events]
+            [ajax.core :as ajax]
             [clojure.string :as string]))
 
 
 (defn get-api-url [term]
-  (let [api {:api-key "BanMePlz"
+  (let [api {:api-key "a7473ecd"
              :hostname "http://www.omdbapi.com"}]
     (str (:hostname api) "?apikey=" (:api-key api) "&t=" term)))
 
 
 (defn build-entry [result]
-  (let [fragment (. js/document createDocumentFragment)
-        entry-title (. js/document createElement "h3")
-        entry-director (. js/document createElement "p")
-        entry-writer (. js/document createElement "p")
-        entry-plot (. js/document createElement "p")
-        entry-release (. js/document createElement "p")
+  (let [fragment (dom/createElement "div")
+        entry-title (dom/createElement "h3")
+        entry-director (dom/createElement "p")
+        entry-writer (dom/createElement "p")
+        entry-plot (dom/createElement "p")
+        entry-release (dom/createElement "p")
         {title :Title
          director :Director
          writer :Writer
          plot :Plot
          release-date :Released} result]
-    (set! (.-textContent entry-title) title)
-    (set! (.-textContent entry-director) (str "Directed by: " director))
-    (set! (.-textContent entry-writer) (str "Written by: " writer))
-    (set! (.-textContent entry-plot) plot)
-    (set! (.-textContent entry-release) (str "Released: " release-date))
+    (dom/setTextContent entry-title title)
+    (dom/setTextContent entry-director (str "Directed by: " director))
+    (dom/setTextContent entry-writer (str "Written by: " writer))
+    (dom/setTextContent entry-plot plot)
+    (dom/setTextContent entry-release) (str "Released: " release-date)
     (reduce (fn [accum current]
               (let [root-node (.getRootNode accum)]
-                (.appendChild root-node current))
+                (dom/appendChild root-node current))
               accum)
-      fragment
-      [entry-title entry-plot entry-director entry-writer entry-release])))
+            fragment
+            [entry-title entry-plot entry-director entry-writer entry-release])))
 
 
 (defn handle-result [result]
-  (let [resultsDiv (. js/document getElementById "search-results")
+  (let [resultsDiv (dom/getElement "search-results")
         entry (build-entry result)]
-    (.appendChild resultsDiv entry)))
+    (dom/appendChild resultsDiv entry)))
 
 
 (defn handle-submit [event]
-  (let [form (.-target event)
-        title (.-value (.querySelector form "#search-title"))]
+  (let [title (dom/getElement "search-title")]
     (.preventDefault event)
     (if-not (string/blank? title)
-      (ajax/GET (get-api-url title) {:handler handle-result
+      (ajax/GET (get-api-url (forms/getValue title)) {:handler handle-result
                                      :response-format (ajax/json-response-format {:keywords? true})}))))
 
 
 (defn main []
-  (let [searchForm (. js/document getElementById "search-form")]
-    (.addEventListener searchForm "submit" handle-submit)))
+  (let [searchForm (dom/getElement "search-form")]
+    (events/listen searchForm events/EventType.SUBMIT handle-submit)))
 
 (main)
